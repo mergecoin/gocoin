d4a21cac5753d58a3f432397ebfb3d6223f570dc
James Adam <james@ventureharbour.com>
2022-01-12 16:46:29 +0000

    feat: add support for applying preamble scores

--- mint_scorer/preambles/simpleScore.go
+++ mint_scorer/preambles/simpleScore.go
@@ -3,5 +3,22 @@ package preambles
 type UnimplementedPreambleScorerExample struct{}
 
 func (s *UnimplementedPreambleScorerExample) ScorePreamble(preamble string) float64 {
-	return 0.0
+	if preamble != "" {
+		return 1.0
+	} else {
+		return 0.0
+	}
 }
+
+//
+////fmt.Println("scoring preamble:\n", preamble)
+//left := "Subject: [PATCH]"
+//right := "---"
+//rx := regexp.MustCompile(`(?s)` + regexp.QuoteMeta(left) + `(.*?)` + regexp.QuoteMeta(right))
+//extracted := rx.FindAllStringSubmatch(preamble, -1)[0][0]
+//
+//fmt.Println("OPERATING:", extracted)
+//isConventional := strings.Split(extracted, "\n")
+//fmt.Println(isConventional)
+//fmt.Println("\n\n")
+//
